1024da7421cb546b5bb1939b4d65edf2f088b600
MarionLeHerisson <marion.hurteau1@gmail.com>
2019-10-21 10:32:20 +0200

    cs fixer

--- src/Processor/FillMissingProcessor.php
+++ src/Processor/FillMissingProcessor.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the BibTex Parser.
+ *
+ * (c) Renan de Lima Barbosa <renandelima@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace RenanBr\BibTexParser\Processor;
 
 class FillMissingProcessor
--- tests/Processor/FillMissingProcessorTest.php
+++ tests/Processor/FillMissingProcessorTest.php
@@ -1,11 +1,22 @@
 <?php
 
+/*
+ * This file is part of the BibTex Parser.
+ *
+ * (c) Renan de Lima Barbosa <renandelima@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace RenanBr\BibTexParser\Test\Processor;
 
 use PHPUnit\Framework\TestCase;
 use RenanBr\BibTexParser\Processor\FillMissingProcessor;
 
+/**
+ * @covers \RenanBr\BibTexParser\Processor\FillMissingProcessor
+ */
 class FillMissingProcessorTest extends TestCase
 {
     public function testWithMissingFields()
@@ -31,7 +42,7 @@ class FillMissingProcessorTest extends TestCase
         ]);
 
         $entry = $processor([
-            'title'       => 'foo',
+            'title' => 'foo',
             'description' => 'bar',
         ]);
 
